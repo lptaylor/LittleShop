d736a40c3c9ed2e0769c6124ea1f9d1c02f9d44a
abenetka <ali.benetka@gmail.com>
2018-12-16 16:14:52 -0700

    Adds all routes for navigation bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+
+  root 'root#index'
+
   resources :users
+  resources :merchants, only: [:index]
+  resources :cart, only: [:index, :show]
+  resources :login, only: [:index]
+  resources :register, only: [:index]
   resources :items
   resources :orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
