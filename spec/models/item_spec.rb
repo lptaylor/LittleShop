ea7e6b64b56bcb6faca388cffd679c0577e6b275
Justin Mauldin <justin.mauldin7@gmail.com>
2018-12-16 14:43:26 -0700

    Adds tests for relationships for items, orders & order_items.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to(:user) }
+    it { should have_many(:order_items) }
+    it { should have_many(:orders).through(:order_items) }
+  end
+  describe 'validations' do
+    it {should validate_presence_of(:item_name)}
+    it {should validate_presence_of(:image)}
+    it {should validate_presence_of(:qty_in_stock)}
+    it {should validate_presence_of(:price)}
+    it {should validate_presence_of(:description)}
+    it {should validate_presence_of(:user_id)}
+    it {should validate_presence_of(:updated_at)}
+    it {should validate_presence_of(:created_at)}
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to(:user) }
+    it { should have_many(:order_items) }
+    it { should have_many(:items).through(:order_items) }
+  end
+  describe 'validations' do
+  end
 end
