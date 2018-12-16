d8a756c9b9929c2210d6e0c0585d0607e0747074
Justin Mauldin <justin.mauldin7@gmail.com>
2018-12-16 14:52:59 -0700

    adds pasing test for user table validations & relationships.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates_presence_of :user_name, :address, :city, :state, :zipcode,
+                        :email, :password, :permissions
+
+  has_many :orders
+  has_many :items
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should have_many(:orders) }
+    it { should have_many(:items) }
+  end
+  describe 'validations' do
+    it {should validate_presence_of(:user_name)}
+    it {should validate_presence_of(:address)}
+    it {should validate_presence_of(:city)}
+    it {should validate_presence_of(:state)}
+    it {should validate_presence_of(:zipcode)}
+    it {should validate_presence_of(:email)}
+    it {should validate_presence_of(:password)}
+    it {should validate_presence_of(:permissions)}
+  end
 end
