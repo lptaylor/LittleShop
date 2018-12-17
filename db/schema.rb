0a161787bdaf8a1882a8eb151115d51bc5ba868c
abenetka <ali.benetka@gmail.com>
2018-12-17 16:08:30 -0700

    Updates migration for password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181217221509) do
+ActiveRecord::Schema.define(version: 20181217230433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 20181217221509) do
     t.string "state"
     t.integer "zipcode"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
