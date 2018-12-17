b6fad07a5d160a215b8872cf5fb4d9262f839f34
abenetka <ali.benetka@gmail.com>
2018-12-17 16:09:33 -0700

    Updates register route

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
 
   root 'root#index'
 
-  resources :users, only:[:new, :index, :create, :show]
+  resources :users, only:[:index, :create, :show]
   resources :merchants, only: [:index]
   resources :cart, only: [:index, :show]
   resources :login, only: [:index]
   resources :items
   resources :orders
+
+  get '/register', to: "users#new"
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
