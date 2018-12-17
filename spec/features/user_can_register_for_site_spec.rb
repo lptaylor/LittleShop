6bbf4c87d0a9d2c4d6d56cb7b1e0fcca575ce906
abenetka <ali.benetka@gmail.com>
2018-12-17 15:31:28 -0700

    Updates test for changes in migrations for login test

--- spec/features/user_can_register_for_site_spec.rb
+++ spec/features/user_can_register_for_site_spec.rb
@@ -4,31 +4,29 @@ describe 'As a visitor' do
   it 'can register to be a user' do
     visit root_path
 
-    user_name = "Wanda123"
+    username = "Wanda123"
     address = "123 AbC St"
     city = "Denver"
     state = "Colorado"
     zipcode = 80128
     email = "jeff@aol.com"
     password = "dogsaregreat"
-    permissions = "user"
 
 
     click_on "Register"
 
     expect(current_path).to eq(new_user_path)
 
-    fill_in :user_user_name, with: user_name
-    fill_in :user_password, with: "password"
-    fill_in :user_address, with: "password"
-    fill_in :user_city, with: "password"
-    fill_in :user_state, with: "password"
-    fill_in :user_zipcode, with: "password"
-    fill_in :user_email, with: "password"
-    fill_in :user_email, with: "password"
+    fill_in :user_username, with: username
+    fill_in :user_password, with: password
+    fill_in :user_address, with: address
+    fill_in :user_city, with: city
+    fill_in :user_state, with: state
+    fill_in :user_zipcode, with: zipcode
+    fill_in :user_email, with: email
 
     click_on "Create User"
 
-    expect(page).to have_content("Welcome, #{user_name}")
+    expect(page).to have_content("Welcome, #{username}")
   end
 end
