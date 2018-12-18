89fe17c64cc5b6ca586aa21d91995f0e0b632a8b
Justin Mauldin <justin.mauldin7@gmail.com>
2018-12-18 16:18:22 -0700

    Updates user model tests to remove username.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,13 +6,12 @@ RSpec.describe User, type: :model do
     it {should have_many(:items)}
   end
   describe 'validations' do
-    it {should validate_uniqueness_of(:username)}
-    it {should validate_presence_of(:username)}
+    it {should validate_uniqueness_of(:email)}
+    it {should validate_presence_of(:email)}
     it {should validate_presence_of(:address)}
     it {should validate_presence_of(:city)}
     it {should validate_presence_of(:state)}
     it {should validate_presence_of(:zipcode)}
-    it {should validate_presence_of(:email)}
     it {should validate_presence_of(:password)}
     it {should validate_presence_of(:role)}
   end
