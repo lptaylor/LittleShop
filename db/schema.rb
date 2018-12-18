77a7dc2240f97a62adbd45fd0c8750859b8773da
Justin Mauldin <justin.mauldin7@gmail.com>
2018-12-18 15:55:21 -0700

    Removes username field from the database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181217230433) do
+ActiveRecord::Schema.define(version: 20181218225345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 20181217230433) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
     t.integer "role", default: 0
   end
 
