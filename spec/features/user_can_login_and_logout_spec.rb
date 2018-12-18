314b7b987b907b8693c2d96dd212107ebf7dc412
abenetka <ali.benetka@gmail.com>
2018-12-17 17:21:55 -0700

    Adds feature test for logout

--- spec/features/user_can_login_and_logout_spec.rb
+++ spec/features/user_can_login_and_logout_spec.rb
@@ -18,4 +18,28 @@ describe 'As a Visitor' do
     expect(page).to have_content("Welcome, #{user.username}")
     expect(page).to_not have_link("Login")
   end
+
+  it 'can logout as a user' do
+    user = User.create(username: "Jeff", address: "123 AbC St", city: "Denver", state: "Colorado", zipcode: 80128, email: "jeff@aol.com", password: "dogsaregreat", role: 0)
+
+    visit root_path
+
+    click_link "Login"
+
+    expect(current_path).to eq(login_path)
+    fill_in :username, with: user.username
+    fill_in :password, with: user.password
+
+    click_button "Login"
+
+    expect(current_path).to eq(profile_path)
+    expect(page).to have_content("Welcome, #{user.username}")
+    expect(page).to_not have_link("Login")
+    expect(page).to have_link("Logout")
+
+    click_link "Logout"
+
+    expect(current_path).to eq(root_path)
+
+  end
 end
