f0624b57263fde5b9071da987e3907808beabf1d
Lance Taylor <lance.taylor98@hotmail.com>
2018-12-19 16:18:24 -0700

    adds Merchant testing for nav. As well as adds .unique to FakerBot

--- spec/factories/item_factory.rb
+++ spec/factories/item_factory.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :item, class: Item do
     association(:user, role: 1)
-    sequence(:item_name) { Faker::Currency.name }
-    sequence(:image_url) { Faker::Dog.name }
+    sequence(:item_name) { Faker::Currency.unique.name }
+    sequence(:image_url) { Faker::Dog.unique.name }
     sequence(:inventory) { Faker::Number.between(1, 1000) }
     sequence(:price) { Faker::Number.between(1, 50_000) }
-    sequence(:description) { Faker::FamousLastWords.last_words  }
+    sequence(:description) { Faker::FamousLastWords.unique.last_words  }
   end
 end
