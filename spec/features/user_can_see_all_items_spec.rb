181fa515c55abc12154cf0944e7a55bbb05da245
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-12-18 17:37:39 -0700

    passing test for all items on index view

---
+++ spec/features/user_can_see_all_items_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'as a user' do
+    before(:each) do
+      @item_1 = create(:item)
+      @item_2 = create(:item)
+      @item_3 = create(:item)
+      @item_4 = create(:item)
+      @item_5 = create(:item)
+      @item_6 = create(:item)
+    end
+    it 'can see all items in items view' do
+
+    visit items_path
+
+    expect(page).to have_content(@item_1.description)
+    expect(page).to have_content(@item_1.item_name)
+    expect(page).to have_content(@item_1.image_url)
+    expect(page).to have_content(@item_1.inventory)
+    expect(page).to have_content(@item_1.merchant_name)
+  end
+end
