3c375c9799239da57aafb2351a86849612deb69a
Justin Mauldin <justin.mauldin7@gmail.com>
2018-12-18 17:40:11 -0700

    Adds test to make sure flash message shows at successful login.

--- spec/features/user_can_register_for_site_spec.rb
+++ spec/features/user_can_register_for_site_spec.rb
@@ -18,9 +18,32 @@ describe 'As a visitor' do
     fill_in :user_city, with: user.city
     fill_in :user_state, with: user.state
     fill_in :user_zipcode, with: user.zipcode
-save_and_open_page
+
     click_on "Create User"
 
     expect(page).to have_content("Welcome, #{user.email}")
+    expect(current_path).to eq(profile_path)
+  end
+  it 'display flash message for a successful registration' do
+    user = build(:user)
+
+    visit root_path
+
+    click_on "Register"
+
+    expect(current_path).to eq(register_path)
+
+    fill_in :user_name, with: user.name
+    fill_in :user_email, with: user.email
+    fill_in :user_password, with: user.password
+    fill_in :user_password_confirmation, with: user.password
+    fill_in :user_address, with: user.address
+    fill_in :user_city, with: user.city
+    fill_in :user_state, with: user.state
+    fill_in :user_zipcode, with: user.zipcode
+
+    click_on "Create User"
+
+    expect(flash[:success]).to be_present
   end
 end
