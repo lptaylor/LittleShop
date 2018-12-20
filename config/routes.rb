6aacee3acf749dafa963892b95e1cf202d923501
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-12-19 17:18:03 -0700

    updated routes for namespace to dashboard, leaving merchant with it's own resources

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,14 @@ Rails.application.routes.draw do
     resources :users, only: [:index]
   end
 
+  get '/dashboard', to: 'dashboard#show'
+  namespace :dashboard do
+  end
+
   resources :users, only: [:create, :edit] do
     resources :orders, only: [:index, :show]
   end
+
   resources :merchants, only: [:index]
   resources :items
   resources :orders
@@ -16,10 +21,12 @@ Rails.application.routes.draw do
   resource :cart, only: [:show]
 
   get '/register', to: "users#new"
+  get '/profile', to: "users#show"
+
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
-  get '/profile', to: "users#show"
+
   get '/profile/orders', to: "orders#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
