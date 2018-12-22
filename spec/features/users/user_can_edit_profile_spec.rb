0f63e158860ae8c5036ec488b0598962391cb1b1
Justin Mauldin <justin.mauldin7@gmail.com>
2018-12-21 23:44:41 -0700

    Adds test for registered user can edit their profile and get flash message.

---
+++ spec/features/users/user_can_edit_profile_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+describe 'as a registered user' do
+  describe 'when logged in' do
+    before(:each) do
+      @user = create(:user)
+
+      visit root_path
+      click_link "Login"
+
+      fill_in :email, with: @user.email
+      fill_in :password, with: @user.password
+
+      click_button "Login"
+    end
+    it 'can edit its own profile' do
+      new_name = "Tom"
+      new_email = "test@test.com"
+      new_password = "ILoveMom"
+      new_address = "125 Main St."
+      new_city = "Zaregerege"
+      new_state = "ZZ"
+      new_zipcode = 12345
+
+      click_link "Edit Profile"
+save_and_open_page
+      fill_in :user_name, with: new_name
+      fill_in :user_email, with: new_email
+      fill_in :user_password, with: new_password
+      fill_in :user_password_confirmation, with: new_password
+      fill_in :user_address, with: new_address
+      fill_in :user_city, with: new_city
+      fill_in :user_state, with: new_state
+      fill_in :user_zipcode, with: new_zipcode
+
+      click_on "Update User"
+save_and_open_page
+      expect(current_path).to eq(profile_path)
+      expect(page).to have_content(new_name)
+      expect(page).to have_content(new_email)
+      expect(page).to have_content(new_address)
+      expect(page).to have_content(new_city)
+      expect(page).to have_content(new_state)
+      expect(page).to have_content(new_zipcode)
+      expect(page).to have_content("Your information has been updated.")
+
+      expect(current_path).to_not eq(profile_edit_path)
+      expect(page).to_not have_content(@user.name)
+      expect(page).to_not have_content(@user.email)
+      expect(page).to_not have_content(@user.address)
+      expect(page).to_not have_content(@user.city)
+      expect(page).to_not have_content(@user.state)
+      expect(page).to_not have_content(@user.zipcode)
+    end
+  end
+end
