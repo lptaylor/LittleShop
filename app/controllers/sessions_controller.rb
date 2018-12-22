8a6fc228f585f8e73c5cc40a64966371ac44d7a8
Justin Mauldin <justin.mauldin7@gmail.com>
2018-12-21 20:40:04 -0700

    Adds code to implement redirect to respective pages if logged in and navigate to login path.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,21 @@
 class SessionsController < ApplicationController
   def new
+    flash_message = flash[:alert] = "You are already logged in!"
+
+    if current_user
+      if current_user.role == 'registered_user'
+        redirect_to profile_path
+        flash_message
+      elsif current_user.role == 'merchant'
+        redirect_to dashboard_path
+        flash_message
+      else current_user.role == 'admin'
+        redirect_to root_path
+        flash_message
+      end
+    else
+      render :new
+    end
   end
 
   def create
