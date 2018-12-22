1f815da00a0a55d0565afc101d3b87271cdbfdec
abenetka <ali.benetka@gmail.com>
2018-12-21 19:01:15 -0700

    Add implementation of feature test for user to view order show page.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
+  include ActionView::Helpers::NumberHelper
+
   validates_presence_of :order_status, :user_id
 
   belongs_to :user
@@ -6,4 +8,15 @@ class Order < ApplicationRecord
   has_many :items, through: :order_items
 
   enum order_status: ["pending", "completed", "cancelled"]
+
+  def total_order_items
+    items.count
+  end
+
+  def total_order_price
+    prices = items.map do |item|
+      item.price
+    end
+    number_to_currency(prices.sum.to_f)
+  end
 end
