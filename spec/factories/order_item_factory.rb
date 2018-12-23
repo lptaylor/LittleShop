5f9559eef76a77f1afb44daef607c10f7302c944
abenetka <ali.benetka@gmail.com>
2018-12-22 19:09:39 -0700

    Updated factory bot to output decimal prices

--- spec/factories/order_item_factory.rb
+++ spec/factories/order_item_factory.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :order_item do
     order
     item
-    sequence(:price) { |n| n + 2  }
+    sequence(:price) { |n| ((n + 2) * 0.75) }
     sequence(:quantity) { |n| n + 1 }
     fulfilled { false }
   end
