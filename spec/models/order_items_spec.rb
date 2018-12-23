a88f01cded0dbcd3c85b0e51a9e030130ea1c7fa
abenetka <ali.benetka@gmail.com>
2018-12-22 19:18:48 -0700

    Adds model test for subtotal method in order_items

--- spec/models/order_items_spec.rb
+++ spec/models/order_items_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe OrderItem, type: :model do
     it { should belong_to(:order) }
     it { should belong_to(:item) }
   end
+
+  describe 'instance methods' do
+    it 'can calculate the subtotal for items on an order' do
+      user = create(:user)
+      item_1 = create(:item)
+      item_2 = create(:item)
+
+      order_1 = create(:order, user: user)
+      order_item_1 = create(:order_item, order: order_1, item: item_1, price: 2.5, quantity: 5)
+
+      expect(order_item_1.subtotal).to eq(12.5)
+    end
+
+  end
+
+
 end
