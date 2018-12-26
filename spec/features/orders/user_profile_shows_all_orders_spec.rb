930379f2a15d85b3ca7838e39884b01e9e000f4f
abenetka <ali.benetka@gmail.com>
2018-12-26 15:45:34 -0700

    Adds test to view all orders on user show page

--- spec/features/orders/user_profile_shows_all_orders_spec.rb
+++ spec/features/orders/user_profile_shows_all_orders_spec.rb
@@ -21,21 +21,41 @@ describe 'As a registered user' do
       item_4 = create(:item)
       item_5 = create(:item)
 
-
       order_1 = create(:order, user: @user)
-      order_1.order_items.create(
-      order_item_1 = create(:order_item, item: item_1)
-      order_item_2 = create(:order_item, item: item_2)
+      order_item_1 = create(:order_item, order: order_1, item: item_1)
+      order_item_2 = create(:order_item, order: order_1, item: item_2)
 
       order_2 = create(:order, user: @user)
       order_item_3 = create(:order_item, order: order_2, item: item_3)
       order_item_4 = create(:order_item, order: order_2, item: item_4)
       order_item_4 = create(:order_item, order: order_2, item: item_5)
 
+      user_2 = create(:user)
+      order_3 =create(:order, user: user_2)
+      order_item_5 = create(:order_item, order: order_3, item: item_1)
+      order_item_6 = create(:order_item, order: order_3, item: item_2)
+
       visit profile_path(@user)
 
-      expect(page).to have_content(order_1.id)
-      expect(page).to have_content(order_2.id)
+      within ".all-orders" do
+        expect(page).to have_content("Order Id: #{order_1.id}")
+        expect(page).to have_content("Order Id: #{order_2.id}")
+        expect(page).to_not have_link("#{order_3.id}")
+      end
+
+      within "#order-0" do
+        expect(page).to have_link("#{order_1.id}")
+        expect(page).to have_content("Order Placed On: #{order_1.created_at.to_date}")
+        expect(page).to have_content("Order Status: #{order_1.order_status}")
+        expect(page).to have_content("Total Items Ordered: #{order_1.total_order_items}")
+      end
+
+      within "#order-1" do
+        expect(page).to have_link("#{order_2.id}")
+        expect(page).to have_content("Order Placed On: #{order_2.created_at.to_date}")
+        expect(page).to have_content("Order Status: #{order_2.order_status}")
+        expect(page).to have_content("Total Items Ordered: #{order_2.total_order_items}")
+      end
     end
   end
 end
