67270925372111847b85d4f7041a36654ead981c
abenetka <ali.benetka@gmail.com>
2018-12-26 16:02:37 -0700

    Updates test to include a variety of order statuses

--- spec/features/orders/user_profile_shows_all_orders_spec.rb
+++ spec/features/orders/user_profile_shows_all_orders_spec.rb
@@ -20,27 +20,32 @@ describe 'As a registered user' do
       item_3 = create(:item)
       item_4 = create(:item)
       item_5 = create(:item)
+      item_6 = create(:item)
 
-      order_1 = create(:order, user: @user)
+      order_1 = create(:order, user: @user, order_status: 0)
       order_item_1 = create(:order_item, order: order_1, item: item_1)
       order_item_2 = create(:order_item, order: order_1, item: item_2)
 
-      order_2 = create(:order, user: @user)
+      order_2 = create(:order, user: @user, order_status: 1)
       order_item_3 = create(:order_item, order: order_2, item: item_3)
       order_item_4 = create(:order_item, order: order_2, item: item_4)
-      order_item_4 = create(:order_item, order: order_2, item: item_5)
+      order_item_5 = create(:order_item, order: order_2, item: item_5)
+
+      order_3 = create(:order, user: @user, order_status: 2)
+      order_item_6 = create(:order_item, order: order_3, item: item_6)
 
       user_2 = create(:user)
-      order_3 =create(:order, user: user_2)
-      order_item_5 = create(:order_item, order: order_3, item: item_1)
-      order_item_6 = create(:order_item, order: order_3, item: item_2)
+      order_4 =create(:order, user: user_2)
+      order_item_5 = create(:order_item, order: order_4, item: item_1)
+      order_item_6 = create(:order_item, order: order_4, item: item_2)
 
       visit profile_path(@user)
 
       within ".all-orders" do
         expect(page).to have_content("Order Id: #{order_1.id}")
         expect(page).to have_content("Order Id: #{order_2.id}")
-        expect(page).to_not have_link("#{order_3.id}")
+        expect(page).to have_content("Order Id: #{order_3.id}")
+        expect(page).to_not have_link("#{order_4.id}")
       end
 
       within "#order-0" do
@@ -56,6 +61,15 @@ describe 'As a registered user' do
         expect(page).to have_content("Order Status: #{order_2.order_status}")
         expect(page).to have_content("Total Items Ordered: #{order_2.total_order_items}")
       end
+
+      within "#order-2" do
+        expect(page).to have_link("#{order_3.id}")
+        expect(page).to have_content("Order Placed On: #{order_3.created_at.to_date}")
+        expect(page).to have_content("Order Status: #{order_3.order_status}")
+        expect(page).to have_content("Total Items Ordered: #{order_3.total_order_items}")
+      end
+
     end
   end
+
 end
