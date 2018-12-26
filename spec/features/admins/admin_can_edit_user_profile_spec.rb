786f2558be9bb4d90357126fd44ce66c9837ec92
abenetka <ali.benetka@gmail.com>
2018-12-26 13:30:02 -0700

    Updates test for admin editing user profile page

--- spec/features/admins/admin_can_edit_user_profile_spec.rb
+++ spec/features/admins/admin_can_edit_user_profile_spec.rb
@@ -4,19 +4,12 @@ describe 'as an Admin' do
   it 'allows admin to edit users profile' do
     admin = create(:user, role: 2)
     user_1 = create(:user)
-    user_2 = create(:user)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
     visit admin_user_path(user_1)
 
     click_link "Edit Profile"
-
-    expect(current_path).to eq(edit_user_path(user_1))
-
-    admin = create(:user, role: 2)
-    user_1 = create(:user)
-    user_2 = create(:user)
 
     new_name = "Wanda"
     new_email = "wanda@aol.com"
@@ -34,9 +27,15 @@ describe 'as an Admin' do
     fill_in :user_city, with: new_city
     fill_in :user_state, with: new_state
     fill_in :user_zipcode, with: new_zipcode
-    binding.pry
+
     click_on "Update User"
-    save_and_open_page
-    expect(current_path).to eq(admin_user_path(user_1))
+
+    expect(page).to have_content("Name: #{new_name}")
+    expect(page).to have_content("Email: #{new_email}")
+    expect(page).to have_content("#{new_name}'s information has been updated.")
+    expect(page).to have_content("Logged in as: #{admin.name}")
+    expect(page).to_not have_content("Your information has been updated.")
+    expect(page).to_not have_content("Name: #{user_1.name}")
+    expect(page).to_not have_content("Email: #{user_1.email}")
   end
 end
