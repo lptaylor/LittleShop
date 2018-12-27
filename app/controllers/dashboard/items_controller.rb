759867b0a26ec03a7311bad20c550fbb793530a1
Lance Taylor <lance.taylor98@hotmail.com>
2018-12-27 14:32:14 -0700

    Fills out methods for Restful Item controller

--- app/controllers/dashboard/items_controller.rb
+++ app/controllers/dashboard/items_controller.rb
@@ -28,12 +28,30 @@ class Dashboard::ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-    
+    if @item.update_attributes(item_params)
+      @item.save
+    else
+      render :edit
+    end
+  end
+
+  def toggle_active(toggle_params)
+    toggle(:enabled).save
   end
 
   def destroy
     item = Item.find(params[:id])
     item.destroy
     redirect_to dashboard_items_path
+  end
+
+  private
+
+  def toggle_params
+    params.require(:item).permit(:enabled)
+  end
+
+  def item_params
+    params.require(:item).permit(:item_name, :image_url, :inventory, :price, :description)
   end
 end
