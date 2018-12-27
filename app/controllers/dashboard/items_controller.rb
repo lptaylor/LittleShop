b7618e70c0b5661e5455c4939cd2dc7dd6d671fa
Lance Taylor <lance.taylor98@hotmail.com>
2018-12-27 14:12:05 -0700

    Adds views for merchant to make new item or edit existing item. As well as controller actions for these

--- app/controllers/dashboard/items_controller.rb
+++ app/controllers/dashboard/items_controller.rb
@@ -4,4 +4,36 @@ class Dashboard::ItemsController < ApplicationController
     current_merchant
   end
 
+  def toggle_active(item)
+    toggle_active(item)
+    redirect dashboard_items_path
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to dashboard_items_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to dashboard_items_path
+  end
 end
