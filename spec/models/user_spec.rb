2be69c236a01e7592ab08ab918aed132a4019304
Justin Mauldin <justin.mauldin7@gmail.com>
2018-12-28 20:27:15 -0700

    Adds model tests for enable, disable & finding registered users.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -45,13 +45,42 @@ RSpec.describe User, type: :model do
   end
   describe 'class methods' do
     it 'will return only active merchants' do
-      create(:user, role: 1)
-      create(:user, role: 1)
-      create(:user, role: 1)
-      create(:user, role: 1, active: false, created_at: 3.days.ago)
-      create(:user, role: 1, active: false, created_at: 3.days.ago)
+      user_1 = create(:user, role: 1)
+      user_2 = create(:user, role: 1)
+      user_3 = create(:user, role: 1)
+      user_4 = create(:user, role: 1, active: false, created_at: 3.days.ago)
+      user_5 = create(:user, role: 1, active: false, created_at: 3.days.ago)
 
-      expect(User.active_merchants.length).to eq(3)
+      expect(User.active_merchants).to eq([user_1, user_2, user_3])
+    end
+
+    it 'will return only registered users' do
+      user_1 = create(:user)
+      user_2 = create(:user)
+      user_3 = create(:user)
+      user_4 = create(:user, role: 1)
+      user_5 = create(:user, role: 1)
+      user_6 = create(:user, role: 2)
+      user_7 = create(:user, role: 2)
+
+      expect(User.registered_users).to eq([user_1, user_2, user_3])
+    end
+  end
+
+  describe 'instance methods' do
+    it 'changes a user from disabled to enabled' do
+      user_1 = create(:user, active: false)
+      user_1.enable
+
+      expect(user_1.active).to eq(true)
+      expect(user_1.active).to_not eq(false)
+    end
+    it 'changes a user from enabled to disabled' do
+      user_1 = create(:user)
+      user_1.disable
+
+      expect(user_1.active).to eq(false)
+      expect(user_1.active).to_not eq(true)
     end
   end
 end
