d5a0081d5e52ac39126daaaa1bc2a3b10f1a96a5
Justin Mauldin <justin.mauldin7@gmail.com>
2018-12-28 22:53:00 -0700

    Adds admin_merchants_path to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :users, only: [:index, :show, :edit, :update]
-    resources :merchants, only: [:show]
+    resources :merchants, only: [:index, :show]
     resources :orders, only: [:show]
   end
 
