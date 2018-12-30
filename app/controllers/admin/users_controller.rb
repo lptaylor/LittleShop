b12e6bda3a360bb6250df9480cd69ce826ac0d6d
Justin Mauldin <justin.mauldin7@gmail.com>
2018-12-30 16:42:01 -0700

    Updates controller for enable & disable of registered users.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -12,4 +12,18 @@ class Admin::UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    
+    if @user.active == false
+      @user.enable
+      flash[:notice] = "#{@user.name}'s account is now enabled"
+      redirect_to admin_users_path
+    elsif  @user.active == true
+      @user.disable
+      flash[:notice] = "#{@user.name}'s account is now disabled"
+      redirect_to admin_users_path
+    end
+  end
+
 end
