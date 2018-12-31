f72a100d06c84dada29873d218b6eac685ebdf93
Justin Mauldin <justin.mauldin7@gmail.com>
2018-12-30 22:28:10 -0700

    Adds update method in admin merchants controller.

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -1,4 +1,16 @@
 class Admin::MerchantsController < ApplicationController
   def show
+    @merchant = User.find(params[:id])
   end
-end 
+
+  def update
+    @merchant = User.find(params[:id])
+
+    if params[:downgrade]
+      @merchant.downgrade_to_user
+      flash[:success] = "Downgrade to User Successful"
+      redirect_to admin_user_path(@merchant.id)
+      return
+    end
+  end
+end
