2e0259d5843162d8e6e3272ebf630962ecd3f1b8
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2019-01-01 14:57:46 -0700

    Added destroy to admin order routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users, only: [:index, :show, :edit, :update]
     resources :merchants, only: [:show, :index, :update]
-    resources :orders, only: [:show]
+    resources :orders, only: [:show, :destroy]
   end
 
   namespace :dashboard do
