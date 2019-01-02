eeb1f56b58fe0db4e9c09c6c4ff87a05a4ccc7ef
abenetka <ali.benetka@gmail.com>
2019-01-01 17:37:00 -0700

    Adds numberhelper to test

--- spec/features/orders/user_can_view_order_show_page_spec.rb
+++ spec/features/orders/user_can_view_order_show_page_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
+include ActionView::Helpers::NumberHelper
+
 describe 'As a registered user' do
   describe 'when I visit an order show page' do
     it 'displays information about the order' do
@@ -18,14 +20,14 @@ describe 'As a registered user' do
       within "#order-item-0" do
         expect(page).to have_content("Name: #{item_1.item_name}")
         expect(page).to have_content("Description: #{item_1.description}")
-        expect(page).to have_content("Price: $#{order_item_1.price}")
+        expect(page).to have_content("Price: #{number_to_currency(order_item_1.price)}")
         expect(page).to have_content("Quantity: #{order_item_1.quantity}")
       end
 
       within "#order-item-1" do
         expect(page).to have_content("Name: #{item_2.item_name}")
         expect(page).to have_content("Description: #{item_2.description}")
-        expect(page).to have_content("Price: $#{(order_item_2.price)}")
+        expect(page).to have_content("Price: #{number_to_currency(order_item_2.price)}")
         expect(page).to have_content("Quantity: #{order_item_2.quantity}")
       end
 
@@ -34,7 +36,7 @@ describe 'As a registered user' do
         expect(page).to have_content("Order Last Updated: #{order_1.updated_at.to_date}")
         expect(page).to have_content("Order Status: #{order_1.order_status}")
         expect(page).to have_content("Number of Items in Order: #{order_1.total_order_items}")
-        expect(page).to have_content("Total Order Price: $#{order_1.total_order_price}")
+        expect(page).to have_content("Total Order Price: #{number_to_currency(order_1.total_order_price)}")
       end
     end
   end
