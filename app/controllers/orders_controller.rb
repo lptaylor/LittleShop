9c5475675449e047786491a0194aafa3e8cd35f3
abenetka <ali.benetka@gmail.com>
2019-01-01 17:30:35 -0700

    Removes unnecessary methods

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,5 @@
 class OrdersController < ApplicationController
 
-  def index
-    @orders = Order.all
-  end
-
-  def show
-    @order = Order.find(params[:id])
-  end
-
   def destroy
     @order = Order.find(params[:id])
     @order.order_status = "cancelled"
