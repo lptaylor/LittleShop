a53f49f925ec3ddea73f1ac6e3befe62955e8eab
Lance Taylor <lance.taylor98@hotmail.com>
2019-01-01 17:14:02 -0700

    Adds basic authoriztion  checks for all user types

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,34 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def current_registered_user?
+    current_user && current_user.registered_user?
+  end
+
+  def current_merchant?
+    current_user && current_user.merchant?
+  end
+
+  def current_admin?
+    current_user && current_user.admin?
+  end
+
+  def require_admin
+    render file: "/public/404" unless current_admin?
+  end
+
+  def require_merchant
+    render file: "/public/404" unless current_merchant?
+  end
+
+  def require_registered_user
+    render file: "/public/404" unless current_registered_user?
+  end
+
+  def authorization
+    render './public/404.html'
+  end
+
   def build_cart
     @cart ||= Cart.new(session[:cart])
   end
